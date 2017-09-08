5690a544f00883c857f148f953201d7920800b8a
Susensio <susensio@gmail.com>
2017-09-08 14:47:38 +0200

    Add calculator
    Minimal implementation using python interpreter

--- calculator.py
+++ calculator.py
@@ -1,3 +1,9 @@
 # Your program will accept a mathematical expression as a string,
 # parse it and output the answer of the expression.
-# For example, "2*3+2-(4+5)" will output 9.
+# For example, "2*3+2-(4+5)" will output -1.
+
+
+# MINIMALISM
+from math import *
+while True:
+    print(eval(input("Enter expression: ")))
