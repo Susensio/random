fc3cabd97645bf79a47d81772fbfc3c460af9bfa
Susensio <susensio@gmail.com>
2017-09-27 17:17:14 +0200

    Improve prime generator

--- idea bag/prime_factors.py
+++ idea bag/prime_factors.py
@@ -1,13 +1,15 @@
 # Develop a program that has the user enter a number and find all the Primer Factors (if there are any) and display them.
 # ---------------- COMPLETED ----------------
 
-
+import sys
+sys.path.append("../")
 import math
 from timer_decorator import timeit
 
 
 def prime_factors(number):
-    return []
+    primes_list = primes_upto(number // 2)
+    return [n for n in factors(number) if n in primes_list]
 
 
 @timeit
@@ -16,10 +18,25 @@ def primes_upto(number):
     for n in range(number):
         if is_prime(int(n + 1)):
             primes.append(n + 1)
-
     return primes
 
 
+def nth_prime(n):
+    primes = prime_generator()
+    for _ in range(n):
+        prime = next(primes)
+    return prime
+
+
+def prime_generator():
+    number = 2
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+
+
+# @timeit
 def factors(number):
     min = 2
     max = int(math.sqrt(number))
@@ -32,11 +49,36 @@ def factors(number):
     return sorted(factors)
 
 
-def is_prime(number):
+@timeit
+def is_prime_slow(number):
     return not factors(number)
 
 
-# Algorithm for finding all prime numbers up to any diven limit
+@timeit
+def is_prime(number):
+    if number == 1:
+        return False
+    elif number < 4:
+        return True
+    elif not number % 2:
+        return False
+    elif number < 9:
+        return True
+    elif not number % 3:
+        return False
+    else:
+        roof = int(math.sqrt(number))
+        factor = 5
+        while factor <= roof:
+            if not number % factor:
+                return False
+            if not number % (factor + 2):
+                return False
+            factor += 6
+    return True
+
+
+# Algorithm for finding all prime numbers up to any given limit
 @timeit
 def sieve_of_eratosthenes(ceil):
     primes = range(2, ceil)
@@ -47,5 +89,8 @@ def sieve_of_eratosthenes(ceil):
 
 
 if __name__ == "__main__":
-    print(primes_upto(1000))
-    print(sieve_of_eratosthenes(1000))
+    # primes_upto(1000)
+    # sieve_of_eratosthenes(1000)
+    # print(prime_factors(13195))
+
+    # print(nth_prime(10001))
