3b23a07dd91f5b0be998aef7769fd233c2066433
Susensio <susensio@gmail.com>
2017-10-20 15:55:50 +0200

    Add tokenizer calculator

--- idea bag/calculator.py
+++ idea bag/calculator.py
@@ -4,6 +4,51 @@
 
 
 # MINIMALISM
-from math import *
-while True:
-    print(eval(input("Enter expression: ")))
+# from math import *
+# while True:
+#     print(eval(input("Enter expression: ")))
+
+
+# TOKENIZER
+Token = namedTuple('Token', ['type', 'value'])
+
+
+def is_digit(ch):
+    return ch in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'}
+
+
+def is_letter(ch):
+    return all(not func(ch) for func in (is_digit, is_operator, is_left_parenthesis, is_right_parenthesis))
+
+
+def is_operator(ch):
+    return ch in {'+', '-', '*', '/', }
+
+
+def is_left_parenthesis(ch):
+    return ch == '('
+
+
+def is_right_parenthesis(ch):
+    return ch == ')'
+
+
+def calculate(text):
+    exp = tokenize(text)
+    while len(exp) > 1:
+        pass
+    return exp
+
+
+def tokenize(text):
+    exp = []
+    number_buffer = []
+    letter_buffer = []
+    for c in text:
+        if is_digit(c):
+            number_buffer.append(c)
+    return exp
+
+
+expression = input("Enter expression: ")
+print(calculate(expression))
