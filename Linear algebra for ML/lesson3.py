71f9ed8a899a791a3447abbe77bba884bac1813f
Susensio <susensio@gmail.com>
2018-04-05 08:58:25 +0200

    PEP formatting

--- Linear algebra for ML/lesson3.py
+++ Linear algebra for ML/lesson3.py
@@ -1,26 +1,27 @@
 # Lesson 03: Vectors
 # Implement vector arithmetic operations such as addition, division, subtraction, and the vector dot product.
 
+
 class Array():
-    
+
     def __init__(self, vector):
         self.vector = [e for e in vector]
-    
+
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__, self.vector)
-    
+
     def __iter__(self):
         yield from self.vector
-        
+
     def __len__(self):
         return len(self.vector)
 
     def __getitem__(self, index):
         return self.vector[index]
-        
+
     def __setitem__(self, index, item):
         self.vector[index] = item
-        
+
     def __add__(self, other):
         """Elemnt-wise addition.
         a + b = (a1+b1, a2+b2, a3+b3)
@@ -31,7 +32,7 @@ class Array():
 
         addition = [e1 + e2 for e1, e2 in zip(self, other)]
         return Array(addition)
-        
+
     def __sub__(self, other):
         """Elemnt-wise subtraction.
         a - b = (a1-b1, a2-b2, a3-b3)
@@ -42,8 +43,6 @@ class Array():
 
         addition = [e1 - e2 for e1, e2 in zip(self, other)]
         return Array(addition)
-    
-
 
     def __mul__(self, other):
         """Element-wise multiplication.
@@ -55,7 +54,7 @@ class Array():
 
         product = [e1 * e2 for e1, e2 in zip(self, other)]
         return Array(product)
-        
+
     def __rmul__(self, number):
         """Element-wise multiplication by number
         n * a = (n*a1, n*a2, n*a3)
@@ -67,14 +66,13 @@ class Array():
         product = [number * e for e in self]
         return Array(product)
 
-
     def __truediv__(self, other):
         """Elemnt-wise division.
         a / b = (a1/b1, a2/b2, a3/b3)
 
         >>> Array([1, 2, 3]) / Array([4, 5, 6])
         Array([0.25, 0.4, 0.5])
-        
+
         >>> Array([1, 2, 3]) / 2
         Array([0.5, 1.0, 1.5])
         """
@@ -82,8 +80,7 @@ class Array():
             division = [e1 / e2 for e1, e2 in zip(self, other)]
         except TypeError:
             division = [e / other for e in self]
-        return Array(division)     
-
+        return Array(division)
 
     def __matmul__(self, other):
         """Vector dot product.
--- Linear algebra for ML/lesson45.py
+++ Linear algebra for ML/lesson45.py
@@ -6,22 +6,23 @@
 
 from lesson3 import Array
 
+
 class Matrix():
-    
+
     def __init__(self, matrix):
         assert all(len(matrix[0]) == len(row) for row in matrix), "Matrix not well formed"
-        
+
         self.matrix = [Array([c for c in row]) for row in matrix]
-        
+
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__, [e.vector for e in self.matrix])
-    
+
     def __iter__(self):
         yield from self.matrix
-        
+
     def __len__(self):
         return len(self.matrix)
-    
+
     @property
     def shape(self):
         """
@@ -30,23 +31,23 @@ class Matrix():
         (2, 3)
         """
         return len(self.matrix), len(self.matrix[0])
-    
+
     def __getitem__(self, pos):
         try:
             row, col = pos
             return self.matrix[row][col]
-        except TypeError: 
+        except TypeError:
             row = pos
             return self.matrix[row]
-        
+
     def __setitem__(self, pos, item):
         try:
             row, col = pos
             self.matrix[row][col] = item
-        except TypeError: 
+        except TypeError:
             row = pos
             self.matrix[row] = item
-        
+
     @property
     def T(self):
         """Traspose.
@@ -55,8 +56,8 @@ class Matrix():
         >>> A.T
         Matrix([[1, 4], [2, 5], [3, 6]])
         """
-        return Matrix([[self[r, c] for r, _ in enumerate(self)] 
-                        for c, _ in enumerate(self.matrix[0])])
+        return Matrix([[self[r, c] for r, _ in enumerate(self)]
+                       for c, _ in enumerate(self.matrix[0])])
 
     def __add__(self, other):
         """Elemnt-wise addition.
@@ -67,10 +68,10 @@ class Matrix():
         Matrix([[2, 4, 6], [8, 10, 12]])
         """
         assert self.shape == other.shape, "Matrices not the same shape"
-        
+
         addition = [[a + b for a, b in zip(Ai, Bi)] for Ai, Bi in zip(self, other)]
         return Matrix(addition)
-        
+
     def __neg__(self):
         """Negation.
 
@@ -89,7 +90,7 @@ class Matrix():
         Matrix([[0, 0, 0], [-2, 0, 2]])
         """
         return self + (- other)
-        
+
     def __matmul__(self, other):
         """Dot product.
 
@@ -97,14 +98,14 @@ class Matrix():
         >>> B = Matrix([[1, 2], [3, 4]])
         >>> A @ B
         Matrix([[7, 10], [15, 22], [23, 34]])
-        
+
         >>> C = Matrix([[1, -1, 2], [0, -3, 1]])
         >>> D = Matrix([[2, 1, 0]])
         >>> C @ D.T
         Matrix([[1], [-3]])
         """
         assert self.shape[1] == other.shape[0], "Matrices not multiplicable"
-        product = [[sum([a*b for a,b in zip(Ai, Bj)])  # Vector dot products A_row · B_col
+        product = [[sum([a * b for a, b in zip(Ai, Bj)])  # Vector dot products A_row · B_col
                     for Bj in other.T.matrix] for Ai in self]
         return Matrix(product)
 
@@ -132,7 +133,7 @@ class Matrix():
         """
         rows, cols = self.shape
         return rows == cols
-        
+
     def __truediv__(self, divisor):
         """Element-wise division.
 
@@ -140,8 +141,8 @@ class Matrix():
         >>> A / 2
         Matrix([[0.5, 1.0], [1.5, 2.0]])
         """
-        return Matrix([[e/divisor for e in row] for row in self])
-        
+        return Matrix([[e / divisor for e in row] for row in self])
+
     def __rmul__(self, factor):
         """Element-wise multiplication.
 
@@ -150,25 +151,24 @@ class Matrix():
         Matrix([[2, 4], [6, 8]])
         """
         return Matrix([[factor * e for e in row] for row in self])
-        
+
     @property
     def adjugate(self):
         """Traspose of cofactor matrix.
-        
+
         >>> A = Matrix([[1, 2], [3, 4]])
         >>> A.adjugate
         Matrix([[4, -2], [-3, 1]])
         """
         assert self.is_square
-        cofactor = Matrix([[(-1)**(r + c) * self.minor(r,c).det for c, _ in enumerate(row_values)] 
-                            for r, row_values in enumerate(self)])
+        cofactor = Matrix([[(-1)**(r + c) * self.minor(r, c).det for c, _ in enumerate(row_values)]
+                           for r, row_values in enumerate(self)])
         return cofactor.T
-        
 
     @property
     def det(self):
         """Recursive calculation.
-        
+
         >>> A = Matrix([[4]])
         >>> A.det
         4
@@ -178,9 +178,9 @@ class Matrix():
         """
         assert self.is_square
         if len(self) == 1:
-            return self[0,0]
+            return self[0, 0]
 
-        return sum([((-1)**col * e) * self.minor(0, col).det 
+        return sum([((-1)**col * e) * self.minor(0, col).det
                     for col, e in enumerate(self.matrix[0])])
 
     def minor(self, row, col):
@@ -190,10 +190,10 @@ class Matrix():
         Matrix([[4]])
         """
         assert self.is_square
-        M = Matrix([[e for c, e in enumerate(row_values) if c != col] 
-                     for r, row_values in enumerate(self) if r != row])
+        M = Matrix([[e for c, e in enumerate(row_values) if c != col]
+                    for r, row_values in enumerate(self) if r != row])
         return M
-        
+
     def __mul__(self, other):
         """Elemnt-wise multiplication.
 
@@ -205,7 +205,7 @@ class Matrix():
         assert self.shape == other.shape, "Matrices not the same shape"
         product = [[a * b for a, b in zip(Ai, Bi)] for Ai, Bi in zip(self, other)]
         return Matrix(product)
-    
+
     def is_symmetric(self, other):
         """
         >>> A = Matrix([[1, 2], [3, 4]])
@@ -214,36 +214,36 @@ class Matrix():
         True
         """
         return self.T == other
-    
+
     def __eq__(self, other):
-        return all([[a == b for a, b in zip(Ai, Bi)] for Ai, Bi in zip(self, other)]) 
+        return all([[a == b for a, b in zip(Ai, Bi)] for Ai, Bi in zip(self, other)])
 
     @property
     def trace(self):
         """Sum of the elements on the main diagonal.
-        
+
         >>> A = Matrix([[1, 2], [3, 4]])
         >>> A.trace
         5
         """
         assert self.is_square
         return sum(self.diagonal)
-        
+
     @property
     def diagonal(self):
         """Main diagonal.
-        
+
         >>> A = Matrix([[1, 2], [3, 4]])
         >>> A.diagonal
         [1, 4]
         """
         assert self.is_square
         return [self[i, i] for i, _ in enumerate(self)]
-        
+
     @property
     def rank(self):
         """ Recursive
-        
+
         >>> A = Matrix([[1, 0, 1], [-2, -3, 1], [3, 3, 0]])
         >>> A.rank
         2
@@ -288,12 +288,12 @@ class Matrix():
                     else:
                         ranks.append(rank)
                 return max(ranks)
-    
+
     @property
     def is_triangular(self):
         assert self.is_square, "Not a square matrix"
         return self.is_upper_triangular or self.is_lower_triangular
-        
+
     @property
     def is_upper_triangular(self):
         """
@@ -306,7 +306,7 @@ class Matrix():
         """
 #        assert self.is_square, "Not a square matrix"
         return all(e == 0 for i, row in enumerate(self) for j, e in enumerate(row) if (i - j) > 0)
-        
+
     @property
     def is_lower_triangular(self):
         """
@@ -319,23 +319,22 @@ class Matrix():
         """
 #        assert self.is_square, "Not a square matrix"
         return all(e == 0 for i, row in enumerate(self) for j, e in enumerate(row) if (j - i) > 0)
-        
-        
+
     def echelon_form(self):
         """Gaussian elimination.
 
         >>> A = Matrix([[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]])
         >>> A.echelon_form()
         Matrix([[2, 1, -1, 8], [0.0, 0.5, 0.5, 1.0], [0.0, 0.0, -1.0, 1.0]])
-        
+
         >>> B = Matrix([[1, 3, 1, 9], [1, 1, -1, 1], [3, 11, 5, 35]])
         >>> B.echelon_form()
         Matrix([[1, 3, 1, 9], [0.0, -2.0, -2.0, -8.0], [0.0, 0.0, 0.0, 0.0]])
-        
+
         >>> C = Matrix([[0, 2, 1, -1], [0, 2, 4, 0], [0, 0, 0, 0]])
         >>> C.echelon_form()
         Matrix([[0, 2, 1, -1], [0.0, 0.0, 3.0, 1.0], [0.0, 0.0, 0.0, 0.0]])
-        
+
         >>> D = Matrix([[0]])
         >>> D.echelon_form()
         Matrix([[0]])
@@ -352,46 +351,45 @@ class Matrix():
                 except ValueError:
                     j = j + 1
             else:
-                for row in range(i+1, len(M)):
+                for row in range(i + 1, len(M)):
                     factor = - M[row, j] / M[i, j]
                     M[row] = M[row] + factor * M[i]
                 i = i + 1
                 j = j + 1
         return M
-        
-        
+
     def reduced_echelon_form(self):
         """Full Gauss-Jordan elimination.
 
         >>> A = Matrix([[1, 3], [1, 4]])
         >>> A.reduced_echelon_form()
         Matrix([[1.0, 0.0], [0.0, 1.0]])
-        
+
         >>> B = Matrix([[1, 3, 1], [1, 1, -1], [3, 11, 5]])
         >>> B.reduced_echelon_form()
         Matrix([[1.0, 0.0, -2.0], [0.0, 1.0, 1.0], [0.0, 0.0, 0.0]])
-        
+
         >>> C = Matrix([[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]])
         >>> C.reduced_echelon_form()
         Matrix([[1.0, 0.0, -2.0], [0.0, 1.0, 1.0], [0.0, 0.0, 0.0]])
         """
         M = Matrix(self).echelon_form()
-        i = M.shape[0]-1
+        i = M.shape[0] - 1
         while i >= 0:
             row_bool = [bool(e) for e in M[i]]
             if any(row_bool):  # If not an empty row
                 j = row_bool.index(True)  # First nonzero element
-                M[i] = M[i] / M[i,j]
+                M[i] = M[i] / M[i, j]
                 for row in range(i):
                     factor = - M[row, j]
                     M[row] = M[row] + factor * M[i]
             i = i - 1
         return M
-        
-        
+
+
 #    def plu(self):
 #        """Main diagonal.
-#        
+#
 #        >>> A = Matrix([[1, 2], [3, 4]])
 #        >>> P, L, U = A.plu
 #        >>> P
@@ -401,10 +399,9 @@ class Matrix():
 #        >>> U
 #        Matrix([[3.0, 4.0], [0.0, 0.66666667]])
 #        """
-#        
+#
 #        return P, L, U
-#    
-            
+#
 
 
 if __name__ == '__main__':
